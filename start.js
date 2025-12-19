914ccb1f63b2096ddfa59791c35559eeddbd376d
zenxmd10 <zenxmd10@gmail.com>
2025-12-20 00:30:17 +0530

    Update start.js

--- start.js
+++ start.js
@@ -2,4 +2,11 @@ const startBot = require('./index')
 const main = require('./main')
 
 console.log('🤖 ZENX-MD Starting...')
-startBot().then(sock => main(sock))()
+
+startBot()
+  .then(sock => {
+    main(sock)
+  })
+  .catch(err => {
+    console.log('❌ Failed to start bot:', err)
+  })
